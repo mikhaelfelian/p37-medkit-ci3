91b495c08945d3c3b73c264b2c42e964573c23b6
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-04-09 22:02:24 +0700

    Added TCPDF
    Added TCPDF Library for better experience use pdf

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -160,5 +160,14 @@
                 0 => '4.7.6',
             ),
         ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => 'ed27e28a4c478f7f4015b5e7e7b1912af9e85f2b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
     ),
 );
