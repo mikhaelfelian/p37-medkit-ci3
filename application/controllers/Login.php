0d76317fcb29eb17108c378d1b3e8e6e7844c4ef
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-04-10 17:40:31 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,5 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <?php
+// Start of Selection
+// Start of Selection
+// Start of Selection
+// Start of Selection
+// Start of Selection
+// Start of Selection
+// Start of Selection
 
 class Login extends CI_Controller {
 
