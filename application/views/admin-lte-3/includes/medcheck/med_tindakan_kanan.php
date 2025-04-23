4c6a054f6b3244da98684686c8a677328b51b318
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-04-23 11:57:59 +0700

    Update med_tindakan_kanan.php

--- application/views/admin-lte-3/includes/medcheck/med_tindakan_kanan.php
+++ application/views/admin-lte-3/includes/medcheck/med_tindakan_kanan.php
@@ -388,8 +388,9 @@
                                         <span class="input-group-text rounded-0"><i class="fas fa-calendar"></i></span>
                                     </div>
                                     <input type="date" id="tgl_lahir" name="tgl_lahir" class="form-control rounded-0"
-                                        value="<?php echo date('m/d/Y', strtotime($sql_pasien->tgl_lahir)) ?>"
-                                        placeholder="mm/dd/yyyy"> 
+                                        value="<?php echo date('Y-m-d', strtotime($sql_pasien->tgl_lahir)) ?>"
+                                        placeholder="mm/dd/yyyy">
+
                                 </div>
                             </div>
                             <div class="form-group">
