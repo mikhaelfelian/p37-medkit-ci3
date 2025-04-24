715e614af74a23bcc67cfa5748535046953a125b
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-04-25 06:56:28 +0700

    Reconfigure Login
    - Changed some setting for Ion Auth library
    - Misconfiguration date of birth format (dd-mm-yyyy)
    - Patient login password using date of birth

--- application/controllers/Medcheck.php
+++ application/controllers/Medcheck.php
@@ -22144,7 +22144,7 @@ public function set_medcheck_lab_adm_save() {
                 // Update password in ion_auth if user exists
                 if (!empty($sql_pasien->id_user)) {
                     // Set password to patient's birth date
-                    $password = $this->tanggalan->tgl_indo_sys($tgl_lahir);
+                    $password = $this->tanggalan->tgl_indo8($tgl_lahir);
                     
                     // Use Ion Auth to update the password
                     $this->ion_auth->update($sql_pasien->id_user, [
